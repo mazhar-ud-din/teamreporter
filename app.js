2785115e4a4978f55024bdd23f1e481c3ec1963f
mazhar-ud-din <81504145+mazhar-ud-din@users.noreply.github.com>
2021-09-11 21:36:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const SignUp = () => {
+    debugger;
+    const SignUpUser = {
+            name: document.getElementById('example').value,
+            email: document.getElementById('example1').value,
+
+            password: document.getElementById('example2').value
+        }
+        // console.log(SignUpUser);
+    localStorage.setItem('SignUpUser', JSON.stringify(SignUpUser));
+    localStorage.getItem('SignUpUser');
+    window.location.assign('file:///C:/Users/Haier/Desktop/TeamReporte/login.html')
+}
+
+const login = () => {
+    debugger;
+    const SignUpUser1 = {
+
+            email: document.getElementById('example3').value,
+
+            password: document.getElementById('example4').value
+        }
+        // console.log(SignUpUser);
+    localStorage.setItem('SignUpUser1', JSON.stringify(SignUpUser1));
+    localStorage.getItem('SignUpUser1');
+    window.location.assign('file:///C:/Users/Haier/Desktop/TeamReporte/popup.html')
+}
+
+const CreateUp = () => {
+    debugger;
+    const SignUpUser2 = {
+            name: document.getElementById('example5').value,
+            category: document.getElementById('example6').value,
+
+            member: document.getElementById('example7').value
+        }
+        // console.log(SignUpUser);
+    localStorage.setItem('SignUpUser2', JSON.stringify(SignUpUser2));
+    localStorage.getItem('SignUpUser2');
+    window.location.assign('file:///C:/Users/Haier/Desktop/TeamReporte/overeiw.html')
+}
